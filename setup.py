f415eeb574832fe4ef7655e235379e6e50e58d81
antoine-de <antoine.desbordes@gmail.com>
2015-09-04 16:05:05 +0200

    add version to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='navitia_wrapper',
     packages=find_packages(),
+    version='0.0.1',
     author_email='dt.ro@canaltp.fr',
     description='Python wrapper around the navitia API (api.navitia.io)',
 )
