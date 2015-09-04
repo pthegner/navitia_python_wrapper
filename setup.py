1babda2806b11aa1feb4aa9ec8908591a23231a0
antoine-de <antoine.desbordes@gmail.com>
2015-09-04 15:31:30 +0200

    add a setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='navitia_wrapper',
+    packages=find_packages(),
+    author_email='dt.ro@canaltp.fr',
+    description='Python wrapper around the navitia API (api.navitia.io)',
+)
+
